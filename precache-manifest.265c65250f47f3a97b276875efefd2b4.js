96abf377e88a3562712d266ffefce835768d9c05
Moinuddin Khan <160009670+Moin06-dev@users.noreply.github.com>
2025-06-30 23:57:11 +0530

    Updates

---
+++ precache-manifest.265c65250f47f3a97b276875efefd2b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8794c86975bde472d55d9181d54365f",
+    "url": "/Interest-Calculator/index.html"
+  },
+  {
+    "revision": "818b193cd5af37586b51",
+    "url": "/Interest-Calculator/static/css/main.01389e88.chunk.css"
+  },
+  {
+    "revision": "108c8b56bd34092a8984",
+    "url": "/Interest-Calculator/static/js/2.c425296a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Interest-Calculator/static/js/2.c425296a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "818b193cd5af37586b51",
+    "url": "/Interest-Calculator/static/js/main.e8764cf5.chunk.js"
+  },
+  {
+    "revision": "431a984f2b2926a77e9b",
+    "url": "/Interest-Calculator/static/js/runtime-main.107e0035.js"
+  }
+]);
